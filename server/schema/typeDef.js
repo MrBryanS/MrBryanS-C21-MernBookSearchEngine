274fa82ea917848e3d820a2e54d74760dab0e94b
Bryan Stauning <bryanstauning@gmail.com>
2023-10-01 18:56:29 -0500

    typeDef.js done

---
+++ server/schema/typeDef.js
@@ -0,0 +1,39 @@
+//allow resolver to access the database
+
+const typeDefs = `#graphql
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+type Auth {
+    token: ID!
+    user: User
+}
+type Query {
+    me: User
+}
+Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(authors: [String], description: String, title: String, bookId: String, image: String, link: String): User
+    removeBook(bookId: String): User
+}
+`;
+
+module.exports = typeDefs;
+
+
+
+
+`
