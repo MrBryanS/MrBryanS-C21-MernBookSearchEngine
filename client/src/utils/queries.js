6fef76f99d247b8b61d5983ab5249ca09b9cfdb5
Bryan Stauning <bryanstauning@gmail.com>
2023-10-11 17:21:55 -0500

    added utils\mutations.js and queries.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,22 @@
+import {
+    gql
+} from '@apollo/client';
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      email
+      username
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
